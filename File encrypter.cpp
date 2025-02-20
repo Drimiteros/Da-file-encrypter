7601dea188c89cdb36c669d2886c62360a376db0
Drimiteros <dimthan2@gmail.com>
2025-02-20 02:36:05 +0200

    Add files via upload

--- File encrypter.cpp
+++ File encrypter.cpp
@@ -5,32 +5,43 @@
 
 using namespace std;
 
-//This function will encrypt a single file
-void encrypt_file(string filePath, string password) {
+int modify_password(string password) {
 	//Convert password to the sum of the decimal value of each letter
 	int sum = 0;
 	for (char c : password) {
 		sum += static_cast<int>(c);
 	}
+	return sum;
+}
 
+//This function will encrypt / decrypt a single file
+void open_file(string filePath, string password, int choice) {
+	//Get the password's sum of the decimal value of each letter and use it to encrypt / decrypt the byte
+	int sum = modify_password(password);
+
+	//Open file to read / temp file to write
 	//tempFile is a temporary file which will save encrypted data of the file
 	fstream file, tempFile;
 	string tempFilePath = "temp.txt";
-
-	//Open file to read / temp file to write
 	file.open(filePath, ios::in | ios::binary);
 	tempFile.open(tempFilePath, ios::out | ios::binary);
 
-	//Read byte by byte through file
-	char byte;
-	while (file.get(byte)) {
-		//Encrypt this byteby adding 1 to it
-		byte += sum;
-		//Save this byte to the temp file
-		tempFile.put(byte);
+	//Encrypt / Decrypt bytes by chunks
+	const size_t buffer_size = 65536; //64 KB chunks
+	vector<unsigned char> buffer(buffer_size);
+	//Process file in chunks
+	while (file.read(reinterpret_cast<char*>(buffer.data()), buffer.size()) || file.gcount() > 0) {
+		size_t bytesRead = file.gcount();
+		for (size_t i = 0; i < bytesRead; i++) {
+			if (choice == 1)
+				buffer[i] = buffer[i] + sum;
+			if (choice == 2)
+				buffer[i] = buffer[i] - sum;
+		}
+		tempFile.write(reinterpret_cast<char*>(buffer.data()), bytesRead);
 	}
 
-	//Close file
+	//Close files
 	file.close();
 	tempFile.close();
 
@@ -38,67 +49,12 @@ void encrypt_file(string filePath, string password) {
 	file.open(filePath, ios::out | ios::binary);
 	tempFile.open(tempFilePath, ios::in | ios::binary);
 
-	//Read byte by byte through temp file 
-	char byte2;
-	while (tempFile.get(byte2)) {
-		//Save this byte into file
-		file.put(byte2);
-	}
+	while (tempFile.read(reinterpret_cast<char*>(buffer.data()), buffer.size()) || tempFile.gcount() > 0)
+		file.write(reinterpret_cast<char*>(buffer.data()), tempFile.gcount());
 
 	//Close files
 	file.close();
 	tempFile.close();
-
-	//Delete temp file
-	remove(tempFilePath.c_str());
-}
-
-//This function will dencrypt a single file
-void decrypt_file(string filePath, string password) {
-	//Convert password to the sum of the decimal value of each letter
-	int sum = 0;
-	for (char c : password) {
-		sum += static_cast<int>(c);
-	}
-
-	//tempFile is a temporary file which will save dencrypted data of the file
-	fstream file, tempFile;
-	string tempFilePath = "temp.txt";
-
-	//Open file to read / temp file to write
-	file.open(filePath, ios::in | ios::binary);
-	tempFile.open(tempFilePath, ios::out | ios::binary);
-
-	//Read byte by byte through file
-	char byte;
-	while (file.get(byte)) {
-		//Dencrypt this byteby adding 1 to it
-		byte -= sum;
-		//Save this byte to the temp file
-		tempFile.put(byte);
-	}
-
-	//Close file
-	file.close();
-	tempFile.close();
-
-	//Open file to write / temp file to read
-	file.open(filePath, ios::out | ios::binary);
-	tempFile.open(tempFilePath, ios::in | ios::binary);
-
-	//Read byte by byte through temp file 
-	char byte2;
-	while (tempFile.get(byte2)) {
-		//Save this byte into file
-		file.put(byte2);
-	}
-
-	//Close files
-	file.close();
-	tempFile.close();
-
-	//Delete temp file
-	remove(tempFilePath.c_str());
 }
 
 int main() {
@@ -111,7 +67,7 @@ int main() {
 	while (go_again == true) {
 		system("cls");
 		cout << "1) Encrypt\n2) Decrypt" << endl;
-		cout << "\nChoise: ";
+		cout << "\nChoice: ";
 		if (cin >> choice && choice == 1) {
 			cout << "Enter a password for encryption: ";
 			cin >> password;
@@ -119,9 +75,6 @@ int main() {
 			cout << "Enter the path of the file you want to encrypt:";
 			cin >> filepath;
 			cout << "Encrypting..." << endl;
-			encrypt_file(filepath, password);
-			system("cls");
-			cout << "Done!" << endl;
 		}
 		else if (choice == 2) {
 			cout << "Enter a password for decryption: ";
@@ -130,10 +83,11 @@ int main() {
 			cout << "Enter the path of the file you want to decrypt:";
 			cin >> filepath;
 			cout << "Decrypting..." << endl;
-			decrypt_file(filepath, password);
-			system("cls");
-			cout << "Done!" << endl;
 		}
+
+		open_file(filepath, password, choice);
+		system("cls");
+		cout << "Done!" << endl;
 
 		go_again = false;
 		cout << "Go again? y/n: ";
